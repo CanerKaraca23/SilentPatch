2b83cc2d2d8a1314d6c7aad078c49b7d75d03e96
CanerKaraca23 <37447503+CanerKaraca23@users.noreply.github.com>
2026-08-02 23:28:29 +0000

    Add temporary logger for CBike::VehicleDamage pattern

--- SilentPatchVC/EntityVC.h
+++ SilentPatchVC/EntityVC.h
@@ -20,7 +20,36 @@ public:
 	CMatrix		m_matrix;
 	void*		clump;
 	uint8_t		m_nType : 3;
-	std::byte	__pad4[11];
+	uint8_t		m_nState : 5;
+
+	uint8_t		bUsesCollision : 1;
+	uint8_t		bEntUFlag02 : 1;
+	uint8_t		bIsStatic : 1;
+	uint8_t		bEntUFlag04 : 1;
+	uint8_t		bEntUFlag05 : 1;
+	uint8_t		bEntUFlag06 : 1;
+	uint8_t		bEntUFlag07 : 1;
+	uint8_t		bRecordCollisions : 1;
+
+	uint8_t		bEntUFlag09 : 1;
+	uint8_t		bExplosionProof : 1;
+	uint8_t		bIsVisible : 1;
+	uint8_t		bHasCollided : 1;
+	uint8_t		bRenderScorched : 1;
+	uint8_t		bHasBlip : 1;
+	uint8_t		bUseLevelSectors : 1;
+	uint8_t		bIsBigBuilding : 1;
+
+	uint8_t		bEntUFlag17 : 1;
+	uint8_t		bBulletProof : 1;
+	uint8_t		bFireProof : 1;
+	uint8_t		bCollisionProof : 1;
+	uint8_t		bMeleeProof : 1;
+	uint8_t		bImmuneToNonPlayerDamage : 1;
+	uint8_t		bEntUFlag23 : 1;
+	uint8_t		bRemoveFromWorld : 1;
+
+	std::byte	__pad4[7];
 	FLAUtils::int16 m_modelIndex;
 
 public:
